31b71279d444d2d830796f6ca859cb7bca8d63a8
pmdasilva <paulo.estudante9904@gmail.com>
2026-07-06 17:30:50 -0300

    feat:nav/header

--- react-with-tailwind-shoes/src/components/Nav.tsx
+++ react-with-tailwind-shoes/src/components/Nav.tsx
@@ -4,9 +4,12 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 
 export default function Nav() {
+
+    const ROUTES = ["Home", "Shop", "About", "Contact"]
+
     return (
         <>
-            <nav className=" flex items-center justify-between">
+            <nav className="flex flex-wrap items-center justify-between">
 
                 {/* Logo */}
                 <a href="#">
@@ -17,7 +20,21 @@ export default function Nav() {
                 <button className="hover:bg-gray-200 rounded-md p-2 focus:ring-2 focus:ring-gray-200">
                     <RxHamburgerMenu size={25} />
                 </button>
-            </nav>
+
+                {/* Mobile Menu */}
+                <div className='w-full'>
+                    <ul className="rounded-lg border  border-gray-100 bg-gray-50 p-4 text-lg">
+                        {ROUTES.map((route, i) => {
+                            return (
+                                <li className={`px-3 py-2 cursor-pointer ${i === 0 ? "bg-blue-600 text-white rounded-lg" : "hover:bg-gray-100"}`} key={route}>
+                                    {route}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </nav >
+
         </>
     )
 }
